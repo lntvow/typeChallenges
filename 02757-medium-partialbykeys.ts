4aafc3e74aa2adf07dbb9716d9ee820a4d6972ce
lntvow <lntvow@qq.com>
2025-05-13 13:55:37 +0800

    20250513

---
+++ 02757-medium-partialbykeys.ts
@@ -0,0 +1,39 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+interface User {
+  name: string
+  age: number
+  address: string
+}
+
+interface UserPartialName {
+  name?: string
+  age: number
+  address: string
+}
+
+interface UserPartialNameAndAge {
+  name?: string
+  age?: number
+  address: string
+}
+
+type cases = [
+  Expect<Equal<PartialByKeys<User, 'name'>, UserPartialName>>,
+  Expect<Equal<PartialByKeys<User, 'name' | 'age'>, UserPartialNameAndAge>>,
+  Expect<Equal<PartialByKeys<User>, Partial<User>>>,
+  // @ts-expect-error
+  Expect<Equal<PartialByKeys<User, 'name' | 'unknown'>, UserPartialName>>
+]
+
+// ============= Your Code Here =============
+type PartialByKeys<T, K extends keyof T = keyof T> = Merge<
+  Omit<T, K> & {
+    [P in K]?: T[P]
+  }
+>
+
+type Merge<T> = {
+  [P in keyof T]: T[P]
+}
